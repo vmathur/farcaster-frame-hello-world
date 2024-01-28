6002fc2bcfe06ec2609679f4e79a01f586dae7d0
vmathur <vmathur@users.noreply.github.com>
2024-01-27 22:22:02 -0800

    messing with button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,7 @@ export default function Home() {
         <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
-        <meta property="fc:frame:button:1" content="Yes" />
-        <meta property="fc:frame:button:2" content="No" />
+        <meta property="fc:frame:button:1" content="World" />
         <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
       </Head>
     </>
