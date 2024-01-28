26d2bb1c770f25dc575260cb2eefdeba7adaaefe
vmathur <vmathur@users.noreply.github.com>
2024-01-28 09:16:19 -0800

    ts to react

---
+++ src/pages/api/post.js
@@ -0,0 +1,22 @@
+import { generateFarcasterFrame } from '@/utils'
+
+export default async function handler (req,res){
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method Not Allowed' })
+    return
+  }
+
+  const signedMessage = req.body;
+  console.log(signedMessage)
+  const choice = signedMessage.untrustedData.buttonIndex
+
+  let html = ''
+
+  if (choice === 1) {
+    html = generateFarcasterFrame("https://farcaster-frame-psi.vercel.app/world.jpg", choice)
+  } else {
+    html = generateFarcasterFrame("https://farcaster-frame-psi.vercel.app/world.jpg", choice)
+  }
+
+  return res.status(200).setHeader('Content-Type', 'text/html').send(html)
+}
