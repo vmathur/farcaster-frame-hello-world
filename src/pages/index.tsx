6588cb6437dbffd43b8fee6e97a9aa2590f482d5
vmathur <vmathur@users.noreply.github.com>
2024-01-27 22:14:29 -0800

    cant stop wont stop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,12 @@ export default function Home() {
     <>
       <Head>
         <meta property="og:title" content="Frame"/>
+        <meta property="og:image" content="https://farcaster-frame-psi.vercel.app/question.jpg"/>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
+        <meta property="fc:frame:image" content="https://farcaster-frame-psi.vercel.app/question.jpg" />
         <meta property="fc:frame:button:1" content="Yes" />
         <meta property="fc:frame:button:2" content="No" />
-        <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
+        <meta property="fc:frame:post_url" content="https://farcaster-frame-psi.vercel.app/api/post" />
       </Head>
     </>
   )
