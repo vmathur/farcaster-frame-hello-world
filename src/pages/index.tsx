8133c4ebb767c39d491aa7178dee988ce125e1b8
vmathur <vmathur@users.noreply.github.com>
2024-01-27 21:52:58 -0800

    trying this

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta property="og:title" content="Frame" />
-        <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame:button:1" content="Yes" />
