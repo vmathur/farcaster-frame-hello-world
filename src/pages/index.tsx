32bdd52573ee1c3f27f216a8825b97406a09b0dd
vmathur <vmathur@users.noreply.github.com>
2024-01-27 22:19:17 -0800

    nuking it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-// import { BASE_URL } from '@/utils'
+import { BASE_URL } from '@/utils'
 import Head from 'next/head'
-
-let BASE_URL = 'https://farcaster-frame-psi.vercel.app/'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <meta property="og:title" content="Frame"/>
-        <meta property="og:image" content="https://farcaster-frame-psi.vercel.app/question.jpg"/>
+        <meta property="og:title" content="Frame" />
+        <meta property="og:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://farcaster-frame-psi.vercel.app/question.jpg" />
+        <meta property="fc:frame:image" content={`${BASE_URL}/question.jpg`} />
         <meta property="fc:frame:button:1" content="Yes" />
         <meta property="fc:frame:button:2" content="No" />
-        <meta property="fc:frame:post_url" content="https://farcaster-frame-psi.vercel.app/api/post" />
+        <meta property="fc:frame:post_url" content={`${BASE_URL}/api/post`} />
       </Head>
     </>
   )
